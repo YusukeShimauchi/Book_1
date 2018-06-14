fa9c46578b976713c914c349793880e24a4069ff
DESKTOP-DNCLGNC\shimauchi <o231034y@gmail.com>
2018-06-14 17:44:13 +0900

    プレゼン前

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'informations#index'
+  get 'informations/product' => 'informations#product'
+  get 'informations/:id/buy' => 'informations#buy'
   resources :informations
 end
